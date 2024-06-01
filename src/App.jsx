e2f331ac69cf7de12013d05e9250f9ac7ca7391a
shrey890 <shrey90@proton.me>
2024-06-01 22:46:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+export default function App() {
+	return (
+		<>
+			<div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+				<Header />
+			</div>
+			<ButtonGradient />
+		</>
+	);
+}
